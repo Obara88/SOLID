5b037b02de770f8d94c0a23a94772fda969bbfc0
Rafael Kenji Obara <kenjiobara@gmail.com>
2018-05-29 00:46:39 -0300

    Dmitri - S

---
+++ Dmitri Nesteruk/1 - S/SRP/SRP/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using static System.Console;
+
+namespace SRP
+{
+    public class Journal
+    {
+        private readonly List<string> entries = new List<string>();
+
+        private static int count = 0;
+
+        public int AddEntry(string text)
+        {
+            entries.Add($"{++count} : {text}");
+            return count; //memento
+        }
+
+        public void RemoveEntry(int index)
+        {
+            entries.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries);
+        }
+
+
+        //Violação SRP !
+        //public void Save(string filename)
+        //{
+        //    File.WriteAllText(filename, ToString());
+        //}
+
+        //public static Journal Load(string filename)
+        //{
+
+        //}
+
+        //public void Load(Uri uri)
+        //{
+
+        //}
+
+    }
+
+    //SOC
+    public class Persistance
+    {
+        public void SaveToFile(Journal j, string filename, bool overwrite = false)
+        {
+            if (overwrite || !File.Exists(filename))
+                File.WriteAllText(filename, j.ToString());
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var j = new Journal();
+            j.AddEntry("I cried today");
+            j.AddEntry("I ate a bug");
+            WriteLine(j);
+
+            var p = new Persistance();
+            var filename = @"c:\temp\journal.txt";
+            p.SaveToFile(j, filename, true);
+            Process.Start(filename);
+        }
+    }    
+}
